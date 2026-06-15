d0361eef46f071fd33b06d67d3473369747d67d0
Raja-Omer-Mustafa <o.mustafa.pseb@gmail.com>
2026-06-16 02:38:59 +0500

    Fix Sell Due Filter in Customer List Fix Payment Broken On Supplier

--- app/Utils/ContactUtil.php
+++ app/Utils/ContactUtil.php
@@ -252,7 +252,7 @@ class ContactUtil extends Util
         if (in_array($type, ['customer', 'both'])) {
             $query->addSelect([
                 DB::raw("SUM(IF(t.type = 'sell' AND t.status = 'final', final_total, 0)) as total_invoice"),
-                DB::raw("SUM(IF(t.type = 'sell' AND t.status = 'final', (SELECT SUM(IF(is_return = 1,-1*amount,amount)) FROM transaction_payments WHERE transaction_payments.transaction_id=t.id), 0)) as invoice_received"),
+                DB::raw("SUM(IF(t.type = 'sell' AND t.status = 'final', COALESCE((SELECT SUM(IF(is_return = 1,-1*amount,amount)) FROM transaction_payments WHERE transaction_payments.transaction_id=t.id), 0), 0)) as invoice_received"),
                 DB::raw("SUM(IF(t.type = 'sell_return', final_total, 0)) as total_sell_return"),
                 DB::raw("SUM(IF(t.type = 'sell_return', (SELECT SUM(amount) FROM transaction_payments WHERE transaction_payments.transaction_id=t.id), 0)) as sell_return_paid"),
                 DB::raw("(SELECT MAX(tp.paid_on) FROM transaction_payments tp INNER JOIN transactions t2 ON tp.transaction_id = t2.id WHERE t2.contact_id = contacts.id AND t2.business_id = $business_id AND t2.type IN ('sell', 'sell_return')) as last_payment_date"),
